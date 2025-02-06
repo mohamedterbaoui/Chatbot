e983db4218fc07ba6ea2855a22a8ead29f75f366
mohamed <m.terbaoui7@gmail.com>
2025-02-06 14:51:21 -0500

    Added server code and frontend

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 let express = require('express');
+let ejs = require('ejs');
 
+let app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.listen(8080);
+
+// localhost:8080
+app.get('/', function(request, response) {
+    response.render('pages/index');
+
+    
+});
